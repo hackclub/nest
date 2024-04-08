6a19210214406e990c3b4906753a3df4603d4d75
aboutdavid <62346025+aboutdavid@users.noreply.github.com>
2024-04-07 23:38:58 -0400

    Add custom domain support to Nest CLI
    Swap file names
    
    Add custom domain support to Nest CLI
    
    Swap file names
    
    Die on failed verification
    
    Validate FDQN & Show "None" instead of nothing

---
+++ cli/remove_domain.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Strip the subdomain of whitespace
+FULL_DOMAIN=$(echo "$1" | xargs)
+
+# "who am i" responds correctly even with sudo
+NEST_USER=$(who am i | awk '{print $1}')
+
+if [ "$NEST_USER" = "root" ]; then
+	echo "Command cannot be run as root!"
+	exit 1
+fi
+
+# Check for existance of subdomain
+if ! grep $FULL_DOMAIN /etc/caddy/Caddyfile &> /dev/null; then
+	echo "You don't have this domain ($FULL_DOMAIN)!"
+	exit 1
+fi
+
+# Set temp Caddyfile
+cat /etc/caddy/Caddyfile > /tmp/root_caddyfile
+
+# Remove configuration
+sed -i "/^$FULL_DOMAIN {/,/^    }/d" /tmp/root_caddyfile
+
+# Validate Caddyfile
+if ! caddy validate --config /tmp/root_caddyfile --adapter caddyfile &> /dev/null; then
+	echo "Error in root Caddyfile! Please contact the Nest admins (@nestadmins) in #nest"
+	exit 1
+fi
+
+# Save Caddyfile
+cat /tmp/root_caddyfile > /etc/caddy/Caddyfile
+rm /tmp/root_caddyfile
+
+# Format Caddyfile
+caddy fmt --overwrite /etc/caddy/Caddyfile
+
+# Reload Caddy instance
+systemctl reload caddy
+
+echo "Removed $FULL_DOMAIN! You may still need to remove it from your Caddy configuration at ~/Caddyfile"
