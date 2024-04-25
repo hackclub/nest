2d03882f84ef1643d9eeca4aaa4d71cdc7ee0280
polypixeldev <79737178+polypixeldev@users.noreply.github.com>
2024-04-25 18:48:46 -0400

    spelling

--- cli/cli.rb
+++ cli/cli.rb
@@ -8,7 +8,7 @@ module NestCLI
   class Subdomain < Thor
     include Thor::Actions
     desc "add <name>", "Add subdomain <name>.youruser.hackclub.app to the Caddyfile"
-    option :no_validate, :type => :boolean, :desc => "Skip validation of your user Caddyfile. Useful if you have custom Caddy plugins not available in the system Caddy installtion."
+    option :no_validate, :type => :boolean, :desc => "Skip validation of your user Caddyfile. Useful if you have custom Caddy plugins not available in the system Caddy installation."
     def add(name)
       run "sudo /usr/local/nest/cli/add_subdomain.sh #{name} #{options[:no_validate] ? "no_validate" : ""}"
     end
@@ -19,7 +19,7 @@ module NestCLI
     end
 
     desc "add42 <name>", "Add subdomain <name>.youruser.hackclub.dn42 to the Caddyfile"
-    option :no_validate, :type => :boolean, :desc => "Skip validation of your user Caddyfile. Useful if you have custom Caddy plugins not available in the system Caddy installtion."
+    option :no_validate, :type => :boolean, :desc => "Skip validation of your user Caddyfile. Useful if you have custom Caddy plugins not available in the system Caddy installation."
     def add42(name)
       run "sudo /usr/local/nest/cli/add42_subdomain.sh #{name} #{options[:no_validate] ? "no_validate" : ""}"
     end
@@ -37,7 +37,7 @@ module NestCLI
   class Domain < Thor
     include Thor::Actions
     desc "add <name>", "Add a custom domain to the Caddyfile"
-    option :no_validate, :type => :boolean, :desc => "Skip validation of your user Caddyfile. Useful if you have custom Caddy plugins not available in the system Caddy installtion."
+    option :no_validate, :type => :boolean, :desc => "Skip validation of your user Caddyfile. Useful if you have custom Caddy plugins not available in the system Caddy installation."
     def add(name)
       run "sudo /usr/local/nest/cli/add_domain.sh #{name} #{options[:no_validate] ? "no_validate" : ""}"
     end
