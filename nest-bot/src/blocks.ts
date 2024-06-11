31cdfabcf4254f4b78e61bbf17b7bdef59aaa092
polypixeldev <79737178+polypixeldev@users.noreply.github.com>
2024-06-10 23:09:25 -0400

    Registration flow part one

--- nest-bot/src/blocks.ts
+++ nest-bot/src/blocks.ts
@@ -195,3 +195,158 @@ export function unregistered_home() {
     ],
   };
 }
+
+export function register_user(profile_name: string) {
+  return {
+    callback_id: "register_user",
+    type: "modal" as const,
+    submit: {
+      type: "plain_text" as const,
+      text: "Submit",
+      emoji: true,
+    },
+    close: {
+      type: "plain_text" as const,
+      text: "Cancel",
+      emoji: true,
+    },
+    title: {
+      type: "plain_text" as const,
+      text: "Register for Nest",
+      emoji: true,
+    },
+    blocks: [
+      {
+        type: "section",
+        text: {
+          type: "plain_text",
+          text: `:wave: Hey ${profile_name}!\n\nPlease enter the required details to register for Nest!`,
+          emoji: true,
+        },
+      },
+      {
+        type: "divider",
+      },
+      {
+        type: "input",
+        block_id: "username",
+        label: {
+          type: "plain_text",
+          text: "Nest username? (This will be your subdomain)",
+          emoji: true,
+        },
+        element: {
+          type: "plain_text_input",
+          action_id: "username_input",
+        },
+      },
+      {
+        type: "input",
+        block_id: "name",
+        label: {
+          type: "plain_text",
+          text: "Full name?",
+          emoji: true,
+        },
+        element: {
+          type: "plain_text_input",
+          action_id: "name_input",
+        },
+      },
+      {
+        type: "input",
+        block_id: "email",
+        label: {
+          type: "plain_text",
+          text: "Email?",
+          emoji: true,
+        },
+        element: {
+          type: "plain_text_input",
+          action_id: "email_input",
+        },
+      },
+      {
+        type: "input",
+        block_id: "ssh_key",
+        label: {
+          type: "plain_text",
+          text: "Public SSH key?",
+          emoji: true,
+        },
+        element: {
+          type: "plain_text_input",
+          action_id: "ssh_key_input",
+          multiline: true,
+        },
+      },
+      {
+        type: "input",
+        block_id: "description",
+        label: {
+          type: "plain_text",
+          text: "What do you plan to use Nest for?",
+          emoji: true,
+        },
+        element: {
+          type: "plain_text_input",
+          action_id: "description_input",
+          multiline: true,
+        },
+      },
+    ],
+  };
+}
+
+export function approval_message(
+  slack_user: string,
+  name: string,
+  username: string,
+  email: string,
+  ssh_key: string,
+  description: string
+) {
+  return [
+    {
+      type: "section",
+      text: {
+        type: "mrkdwn",
+        text: `You have a new request from <@${slack_user}>`,
+      },
+    },
+    {
+      type: "section",
+      text: {
+        type: "mrkdwn",
+        text: `*Full Name:*\n${name}\n*Username:*\n${username}\n*Email:* ${email}\n*SSH Public Key:* \`${ssh_key}\`\n*Description:* ${description}`,
+      },
+    },
+    {
+      type: "actions",
+      elements: [
+        {
+          type: "button",
+          text: {
+            type: "plain_text",
+            emoji: true,
+            text: "Approve",
+          },
+          style: "primary",
+          value: "approve",
+          action_id: "approve_action",
+        },
+        {
+          type: "button",
+          text: {
+            type: "plain_text",
+            emoji: true,
+            text: "Deny",
+          },
+          style: "danger",
+          value: "deny",
+          action_id: "deny_action",
+        },
+      ],
+    },
+  ];
+}
