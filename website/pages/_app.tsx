6d469014ae8458407d7a74fa6a81b8da72d02894
polypixeldev <79737178+polypixeldev@users.noreply.github.com>
2024-06-21 17:40:28 -0400

    Add title and favicon

--- website/pages/_app.tsx
+++ website/pages/_app.tsx
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { DM_Mono } from "next/font/google";
+import Head from "next/head";
 
 const dm_mono = DM_Mono({
   weight: ["400", "500"],
@@ -11,6 +12,10 @@ const dm_mono = DM_Mono({
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div className={`${dm_mono.variable}`}>
+      <Head>
+        <title>Nest</title>
+        <link rel="icon" type="image/png" href="/favicon.png" />
+      </Head>
       <Component {...pageProps} />
     </div>
   );
