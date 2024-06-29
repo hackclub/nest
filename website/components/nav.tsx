e6c2ce4b68f6466b6dca37e13b9ec04117c6adae
Kartikey S. Chauhan <skartikey314@gmail.com>
2024-06-29 18:02:40 +0530

    Make navbar responsive

--- website/components/nav.tsx
+++ website/components/nav.tsx
@@ -2,17 +2,33 @@ import Image from "next/image";
 import Link from "next/link";
 import { motion } from "framer-motion";
 import { FaHome, FaServer, FaBook } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa6";
 
 const navItems = [
-  { href: "/", icon: <FaHome className="mr-2" />, text: "Home" },
-  { href: "https://guides.hackclub.app/", icon: <FaBook className="mr-2" />, text: "Wiki" },
-  { href: "https://status.hackclub.app/", icon: <FaServer className="mr-2" />, text: "Status" },
+  { href: "/", icon: <FaHome className="hidden md:block md:mr-2 text-2xl" />, text: "Home" },
+  { href: "https://guides.hackclub.app/", icon: <FaBook className="sm:mr-0 md:mr-2 text-xl" />, text: "Wiki" },
+  { href: "https://status.hackclub.app/", icon: <FaServer className="sm:mr-0 md:mr-2 text-xl" />, text: "Status" },
+  { href: "https://github.com/hackclub/nest/", icon: <FaGithub className="sm:mr-0 md:hidden" />},
 ];
 
 export default function Nav() {
   return (
-    <nav className="flex items-center justify-between px-4 py-4 md:px-16 relative">
-      <div className="absolute left-4 top-1/2 transform -translate-y-1/2 flex items-center gap-x-4">
+    <nav className="flex items-center justify-between px-4 py-4 md:px-16 relative border-b-2 border-violet-500 md:border-transparent md:bg-transparent">
+      <div className="flex items-center gap-x-4 md:hidden">
+        <Link href={"/"}>
+          <Image src={"/nest.svg"} alt="nest logo" width={70} height={70} />
+        </Link>
+      </div>
+
+      <div className="absolute right-4 top-1/2 transform -translate-y-1/2 flex items-center gap-x-4 lg:hidden">
+        {navItems.map((item, index) => (
+          <Link key={index} href={item.href} className="text-white">
+            {item.icon}
+          </Link>
+        ))}
+      </div>
+
+      <div className="absolute left-4 top-1/2 transform -translate-y-1/2 flex items-center gap-x-4 hidden lg:block">
         <Link href={"https://hackclub.com/"}>
           <motion.div
             initial={{ y: "-100%" }}
@@ -31,7 +47,7 @@ export default function Nav() {
         </Link>
       </div>
 
-      <div className="ml-24 md:ml-32 flex items-center justify-center gap-x-4 md:gap-x-12 font-dm-mono text-base font-medium text-white md:text-lg lg:text-2xl">
+      <div className="hidden lg:flex lg:ml-24 md:ml-32 items-center justify-center gap-x-4 md:gap-x-12 font-dm-mono text-base font-medium text-white md:text-lg lg:text-2xl">
         <Link className="flex-shrink-0" href={"/"}>
           <Image src={"/nest.svg"} alt="nest logo" width={85} height={85} />
         </Link>
@@ -42,12 +58,12 @@ export default function Nav() {
             className="hover:text-HCBlue flex items-center hover:underline"
             href={item.href}
           >
-            {item.icon} {item.text}
+            {item.icon} <span className="hidden md:inline">{item.text}</span>
           </Link>
         ))}
       </div>
 
-      <div className="flex items-center justify-end gap-x-4 md:gap-x-10">
+      <div className="hidden lg:flex items-center justify-end gap-x-4 md:gap-x-10">
         <a
           href="https://identity.hackclub.app"
           className="rounded-lg border-2 border-HCPurple px-4 py-1.5 md:px-8 font-dm-mono text-base font-medium text-HCPurple transition-all duration-300 hover:scale-110 hover:bg-HCPurple hover:text-white md:text-lg lg:text-xl"
