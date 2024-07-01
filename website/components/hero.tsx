464fd10f5792e2dc7d335c4ffd40785411edacf4
Kartikey S. Chauhan <skartikey314@gmail.com>
2024-07-01 08:55:00 +0530

    I DID NOT KNOW OVERFLOW WAS NOT SET TO HIDDEN

--- website/components/hero.tsx
+++ website/components/hero.tsx
@@ -32,7 +32,7 @@ export default function Hero() {
             Read the Docs <FaArrowRight className="ml-2 mt-1" />
           </a>
         </div>
-        <pre className="font-mono text-[4px] lg:hidden mx-16">{`
+        <pre className="font-mono text-[6px] lg:hidden px-8">{`
                                         ▓▓▓▓▓▓                                          
                                     ▓▓▓▓░░░░░░▓▓▓▓                                      
                                 ▓▓▓▓░░░░░░░░░░░░▒▒▓▓██                                  
--- website/pages/index.tsx
+++ website/pages/index.tsx
@@ -6,7 +6,7 @@ import Footer from "@/components/footer";
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-bg">
+    <main className="min-h-screen bg-bg overflow-hidden">
       <Nav />
       <Hero />
       <Showcase />
