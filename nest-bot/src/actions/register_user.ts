58a7405b017b6bb46ba08d4dcbfd80077455cd2d
David M <62346025+aboutdavid@users.noreply.github.com>
2024-07-01 14:46:42 -0400

    Send the user a DM rather than responding.

--- nest-bot/src/actions/register_user.ts
+++ nest-bot/src/actions/register_user.ts
@@ -3,7 +3,7 @@ import Slack from "@slack/bolt";
 import register_form from "../blocks/register_form.js";
 
 export function register_user(app: Slack.App) {
-  app.action("register_user", async ({ ack, body, client, say }) => {
+  app.action("register_user", async ({ ack, body, client }) => {
     ack();
 
     if (body.type !== "block_actions") {
@@ -29,7 +29,10 @@ export function register_user(app: Slack.App) {
       redirect: "follow"
     })).text()
 
-    if (!verificationResponse.includes("Eligible L1") || !verificationResponse.includes("Eligible L2")) await say("Before you can sign up for nest, you'll need to verify that you are a student. Please see https://forms.hackclub.com/eligibility for more details.")
+    if (!verificationResponse.includes("Eligible L1") || !verificationResponse.includes("Eligible L2")) return await app.client.chat.postMessage({
+    channel: body.user.id,
+    text: "Before you can sign up for nest, you'll need to verify that you are a student with Hack Club. Please see https://forms.hackclub.com/eligibility for more details."
+  })
     await client.views.open({
       trigger_id: body.trigger_id,
       view: register_form(
