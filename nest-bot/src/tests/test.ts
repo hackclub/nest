ae2c3eee8178818ae8a3789416976d7d58ee78c4
Kieran Klukas <92754843+kcoderhtml@users.noreply.github.com>
2024-07-20 11:44:43 -0400

    feat: Add /etc/shells file and test for shell filtering
    This commit adds the /etc/shells file to the repository, which contains a list of valid login shells. It also includes a test file that checks if the shells retrieved from the get_shells utility function match the expected list of shells. This addition improves the reliability and accuracy of the shell filtering process in the Nest Bot application.

---
+++ nest-bot/src/tests/test.ts
@@ -0,0 +1,17 @@
+import getShells from "../util/get_shells.js";
+
+(async () => {
+    const shells = await getShells("/bin/bash", "shells");
+    const properShells = ["/bin/sh", "/bin/bash", "/bin/rbash", "/bin/dash", "/usr/bin/tmux", "/bin/zsh", "/bin/csh",
+        "/bin/ksh93", "/bin/rksh93", "/usr/bin/fish", "/bin/tcsh", "/usr/bin/screen"
+    ];
+    console.log("gotten shells", shells);
+    console.log("proper shells", properShells);
+
+    console.log("\n---\n")
+
+    const isMatch = shells.every(shell => properShells.includes(shell));
+    console.log(`The shell filtering is a match? ${isMatch ? '\x1b[32mtrue! 🎉' : '\x1b[31mfalse :('}\x1b[0m`);
+
+    console.log("\n---")
+})();
