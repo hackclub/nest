52c45a7812d59c65ed823a30810481e40a605e94
polypixeldev <79737178+polypixeldev@users.noreply.github.com>
2024-08-03 20:50:44 -0400

    Fix edit shell not being included in exports

--- nest-bot/src/actions/index.ts
+++ nest-bot/src/actions/index.ts
@@ -3,3 +3,4 @@ export { edit_full_name } from "./edit_full_name.js";
 export { edit_email } from "./edit_email.js";
 export { approve } from "./approve.js";
 export { deny } from "./deny.js";
+export { edit_shell } from "./edit_shell.js";
--- nest-bot/src/views/index.ts
+++ nest-bot/src/views/index.ts
@@ -1,3 +1,4 @@
 export { register_user } from "./register_user.js";
 export { edit_full_name } from "./edit_full_name.js";
 export { edit_email } from "./edit_email.js";
+export { edit_shell } from "./edit_shell.js";
