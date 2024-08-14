80c0da32f3ee31d787b9e66573f1f5c2ec7ecab4
polypixeldev <79737178+polypixeldev@users.noreply.github.com>
2024-08-13 22:06:11 -0400

    add a bunch of general metadata

--- website/pages/_app.tsx
+++ website/pages/_app.tsx
@@ -15,6 +15,18 @@ export default function App({ Component, pageProps }: AppProps) {
       <Head>
         <title>Nest</title>
         <link rel="icon" type="image/png" href="/favicon.png" />
+        <meta
+          name="description"
+          content="Nest is a free Linux server for high-schoolers to host their projects on, from Hack Club."
+        />
+        <meta property="og:type" content="website" />
+        <meta name="apple-mobile-web-app-title" content="Nest" />
+        <meta property="og:site_name" content="Nest" />
+        <meta
+          property="og:title"
+          content="Nest - a free Linux server from Hack Club"
+        />
+        <meta property="og:image" content="/nest.svg" />
       </Head>
       <Component {...pageProps} />
     </div>
