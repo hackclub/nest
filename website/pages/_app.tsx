ffed16caa8140d1c3a09948ba6bfa51870d12cf1
polypixeldev <79737178+polypixeldev@users.noreply.github.com>
2024-08-13 22:11:29 -0400

    Add different metadata for projects page

--- website/pages/_app.tsx
+++ website/pages/_app.tsx
@@ -13,16 +13,25 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <div className={`${dm_mono.variable}`}>
       <Head>
-        <title>Nest</title>
+        <title key="title">Nest</title>
         <link rel="icon" type="image/png" href="/favicon.png" />
+
+        <meta
+          key="meta-title"
+          name="title"
+          content="Nest - a free Linux server from Hack Club"
+        />
         <meta
           name="description"
           content="Nest is a free Linux server for high-schoolers to host their projects on, from Hack Club."
         />
-        <meta property="og:type" content="website" />
+
         <meta name="apple-mobile-web-app-title" content="Nest" />
+
+        <meta property="og:type" content="website" />
         <meta property="og:site_name" content="Nest" />
         <meta
+          key="og:title"
           property="og:title"
           content="Nest - a free Linux server from Hack Club"
         />
