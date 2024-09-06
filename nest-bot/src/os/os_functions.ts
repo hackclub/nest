85458fdf1c4a523f703e61b60c2de356ea1bca53
David M <62346025+aboutdavid@users.noreply.github.com>
2024-09-05 22:33:22 -0400

    Migrate nest bot to new caddy manager

--- nest-bot/src/os/os_functions.ts
+++ nest-bot/src/os/os_functions.ts
@@ -1,14 +1,8 @@
 import { readFile, writeFile } from "fs/promises";
-import { exec, ExecException } from "child_process";
+import { exec, ExecException, execSync } from "child_process";
 
 export async function add_root_caddyfile_config(username: string) {
-  const templateConfig = (
-    await readFile("./src/os/templates/root_caddyfile_config.txt")
-  ).toString("utf8");
-
-  const newConfig = templateConfig.replace(/<username>/g, username);
-
-  await writeFile("/etc/caddy/Caddyfile", newConfig, { flag: "a" });
+  execSync(`nest caddy add ${username}.hackclub.app --user ${username}`)
 }
 
 function log_output(err: ExecException | null, stdout: string, stderr: string) {
