e29f8042a33f18b341426e329159c9c6cabccc6e
David M <62346025+aboutdavid@users.noreply.github.com>
2024-09-15 21:02:43 -0400

    Force bun and bun version 1.1.25+

--- cli/index.js
+++ cli/index.js
@@ -1,8 +1,17 @@
-#!/usr/bin/env bun
+#!/usr/local/bin/bun
+if (typeof Bun == "undefined") {
+    console.log("The Nest CLI requires bun (bun.sh) to run.")
+    process.exit(1)
+}
 const { Command } = require('commander');
 const program = new Command();
 const { execSync } = require('child_process');
+const { semver } = require("bun")
 
+if (!semver.satisfies(Bun.version, ">1.1.25")) {
+    console.log(`The Nest CLI requires a bun version greater than 1.1.25 to run. You can run "bun upgrade" to upgrade it.`)
+    process.exit(1)
+}
 function run(command) {
     try {
         console.log(`> ${command}`)
