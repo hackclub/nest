80361a7767f9f6c2d3a162b7d86bcb1bf7b3d181
polypixeldev <79737178+polypixeldev@users.noreply.github.com>
2024-09-17 17:04:53 -0400

    Add back global metadata

--- website/components/layout.tsx
+++ website/components/layout.tsx
@@ -1,14 +1,33 @@
+import Head from "next/head";
 import Nav from "@/components/nav";
 import Footer from "@/components/footer";
 import NestMascot from "@/components/nestMascot";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="min-h-screen bg-bg font-dm-mono text-white">
-      <Nav />
-      <main className="scrollbar-custom overflow-hidden">{children}</main>
-      <Footer />
-      <NestMascot hoverImageSrc="/favicon.png" defaultImageSrc="/nest.png" />
-    </div>
+    <>
+      <Head>
+        <title>Nest</title>
+        <link rel="icon" type="image/png" href="/favicon.png" />
+        <meta
+          name="description"
+          content="Nest is a free Linux server for high-schoolers to host their projects on, from Hack Club."
+        />
+        <meta property="og:type" content="website" />
+        <meta name="apple-mobile-web-app-title" content="Nest" />
+        <meta property="og:site_name" content="Nest" />
+        <meta
+          property="og:title"
+          content="Nest - a free Linux server from Hack Club"
+        />
+        <meta property="og:image" content="/nest.svg" />
+      </Head>
+      <div className="min-h-screen bg-bg font-dm-mono text-white">
+        <Nav />
+        <main className="scrollbar-custom overflow-hidden">{children}</main>
+        <Footer />
+        <NestMascot hoverImageSrc="/favicon.png" defaultImageSrc="/nest.png" />
+      </div>
+    </>
   );
 }
