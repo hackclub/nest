1301ea5ecb0cf731843d61863e608027140976fb
Annabel Q <84078890+phthallo@users.noreply.github.com>
2024-09-22 00:28:12 +0930

    fix: nest mascot obscuring content  (#90)
    * fix: nest mascot obscuring content (hackclub/nest#83)
    
    * style: remove comma, whitespace

--- website/components/layout.tsx
+++ website/components/layout.tsx
@@ -1,14 +1,22 @@
 import Nav from "@/components/nav";
 import Footer from "@/components/footer";
 import NestMascot from "@/components/nestMascot";
+import { useEffect, useRef, useState } from "react";
+import { useInView } from "framer-motion";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
+  const ref = useRef<HTMLDivElement>(null);
+  const [isInView, setIsInView] = useState(useInView(ref));
+  useEffect(() => {
+    setIsInView(!isInView)
+  }, [useInView(ref)])
+
   return (
     <div className="min-h-screen bg-bg font-dm-mono text-white">
       <Nav />
       <main className="scrollbar-custom overflow-hidden">{children}</main>
-      <Footer />
-      <NestMascot hoverImageSrc="/favicon.png" defaultImageSrc="/nest.png" />
+      <Footer ref = {ref} />
+      <NestMascot visible = {isInView} hoverImageSrc="/favicon.png" defaultImageSrc="/nest.png" />
     </div>
   );
 }
