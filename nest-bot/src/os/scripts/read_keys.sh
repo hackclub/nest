3188cf9f57e72d259ed9175aae3f06410992b7a3
polypixeldev <79737178+polypixeldev@users.noreply.github.com>
2024-09-26 16:28:02 -0400

    last quickfix

--- nest-bot/src/os/scripts/read_keys.sh
+++ nest-bot/src/os/scripts/read_keys.sh
@@ -5,6 +5,7 @@ if [ ! -f "/home/$1/.ssh/authorized_keys" ]; then
 
   touch /home/$1/.ssh/authorized_keys
   chmod -R 700 /home/$1/.ssh
+  chown -R $1:$1 /home/$1/.ssh
 fi
 
 cat /home/$1/.ssh/authorized_keys
--- nest-bot/src/os/scripts/set_keys.sh
+++ nest-bot/src/os/scripts/set_keys.sh
@@ -5,6 +5,7 @@ if [ ! -f "/home/$1/.ssh/authorized_keys" ]; then
 
   touch /home/$1/.ssh/authorized_keys
   chmod -R 700 /home/$1/.ssh
+  chown -R $1:$1 /home/$1/.ssh
 fi
 
 cp /home/$1/.ssh/authorized_keys /home/$1/.ssh/authorized_keys.bak
