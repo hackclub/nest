538fb77cd50dee8922c53c2d44cff8021fd6e179
Samuel Fernandez <79737178+polypixeldev@users.noreply.github.com>
2024-09-26 10:56:09 -0400

    Add back global metadata (#85)

--- website/components/layout.tsx
+++ website/components/layout.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import Nav from "@/components/nav";
 import Footer from "@/components/footer";
 import NestMascot from "@/components/nestMascot";
@@ -9,15 +10,37 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   const [isInView, setIsInView] = useState(false);
   const viewChange = useInView(ref);
   useEffect(() => {
-    setIsInView(i => !i)
-  }, [viewChange])
+    setIsInView((i) => !i);
+  }, [viewChange]);
 
   return (
-    <div className="min-h-screen bg-bg font-dm-mono text-white">
-      <Nav />
-      <main className="scrollbar-custom overflow-hidden">{children}</main>
-      <Footer ref = {ref} />
-      <NestMascot visible = {isInView} hoverImageSrc="/favicon.png" defaultImageSrc="/nest.png" />
-    </div>
+    <>
+      <Head>
+        <title>Nest</title>
+        <link rel="icon" type="image/png" href="/favicon.png" />
+        <meta
+          name="description"
+          content="Nest is a free Linux server for high-schoolers to host their projects on, from Hack Club."
+        />
+        <meta property="og:type" content="website" />
+        <meta name="apple-mobile-web-app-title" content="Nest" />
+        <meta property="og:site_name" content="Nest" />
+        <meta
+          property="og:title"
+          content="Nest - a free Linux server from Hack Club"
+        />
+        <meta property="og:image" content="/nest.svg" />
+      </Head>
+      <div className="min-h-screen bg-bg font-dm-mono text-white">
+        <Nav />
+        <main className="scrollbar-custom overflow-hidden">{children}</main>
+        <Footer ref={ref} />
+        <NestMascot
+          visible={isInView}
+          hoverImageSrc="/favicon.png"
+          defaultImageSrc="/nest.png"
+        />
+      </div>
+    </>
   );
 }
