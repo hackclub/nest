4e4781eb4a9f8e72640c7378f404f6f43af85fbf
polypixeldev <me@sfernandez.dev>
2025-05-28 23:55:25 -0400

    properly register view

--- quetzal/src/views/index.ts
+++ quetzal/src/views/index.ts
@@ -4,3 +4,4 @@ export { edit_email } from "./edit_email.js";
 export { edit_shell } from "./edit_shell.js";
 export { new_ssh_key } from "./new_ssh_key.js";
 export { ssh_keys_view } from "./ssh_keys_view.js";
+export { new_verification_code } from "./new_verification_code.js";
