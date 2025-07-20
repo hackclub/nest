db15be5a6892e97d39cb110363a72a70ad55a4c9
Cyteon <129582290+cyteon@users.noreply.github.com>
2025-07-20 15:59:02 +0200

    Change the link to the new verification system

--- quetzal/src/blocks/verification_required.ts
+++ quetzal/src/blocks/verification_required.ts
@@ -22,7 +22,7 @@ export default function verification_required() {
         type: "section",
         text: {
           type: "mrkdwn",
-          text: "Before you can sign up for Nest, you'll need to verify that you are a student with Hack Club. Please go to https://forms.hackclub.com/eligibility to start.\n\n_Note that this form is processed by Hack Club HQ (not Nest), so if you have any questions about how to complete it, please email verifications@hackclub.com. It may take a while for the team to process your submission._",
+          text: "Before you can sign up for Nest, you'll need to verify that you are a student with Hack Club. Please go to https://identity.hackclub.com to start.\n\n_Note that this form is processed by Hack Club HQ (not Nest), so if you have any questions about how to complete it, please email team@hackclub.com. It may take a while for the team to process your submission._",
         },
       },
       {
