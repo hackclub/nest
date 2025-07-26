8434a7b7d32b8d2e6b7433f10ff73000f5e7fc6a
h <9pfs@amcforum.wiki>
2025-07-26 13:30:53 -0700

    partially done

--- quetzal/src/views/submit_code.ts
+++ quetzal/src/views/submit_code.ts
@@ -5,24 +5,30 @@ import register_form from "../blocks/register_form.js";
 
 export function submit_code(app: Slack.App) {
   app.view("submit_code", async ({ ack, body, view, client }) => {
+    console.time("submit_code");
+
     try {
       const codeString = view.state.values.code.code_input.value;
 
       if (!codeString) {
+        console.timeEnd("submit_code");
         return ack();
       }
-
+      console.timeLog("submit_code", "prisma-codes-findfirst-pre");
       const code = await prisma.codes.findFirst({
         where: {
           code: codeString,
         },
       });
+      console.timeLog("submit_code", "prisma-codes-findfirst-post");
 
+      console.timeLog("submit_code", "prisma-ack-code-invalid-pre-nopost");
       if (
         !code ||
         !code.valid ||
         (code.expiry !== null && code.expiry.valueOf() < new Date().valueOf())
       ) {
+        console.timeEnd("submit_code");
         return await ack({
           response_action: "errors",
           errors: {
@@ -31,9 +37,11 @@ export function submit_code(app: Slack.App) {
         });
       }
 
+      console.timeLog("submit_code", "slack-user-lookup-pre");
       const profileRes = await client.users.profile.get({
         user: body.user.id,
       });
+      console.timeLog("submit_code", "slack-user-lookup-post");
 
       await ack({
         response_action: "update",
