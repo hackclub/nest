c58d436c30b49925eb8f7662f061946c3cf6d2e3
cyteon <me@cyteon.dev>
2026-06-01 20:55:21 +0200

    fix user count

--- website/pages/api/userCount.ts
+++ website/pages/api/userCount.ts
@@ -11,6 +11,18 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseData>,
 ) {
-  // TODO: replace with actual data
-  res.status(200).json({ count: 367 });
+  try {
+    const result = await fetch("https://dashboard.hackclub.app/api/stats");
+
+    if (!result.ok) {
+      return res.status(200).json({ count: 477 }); // user count when i made this code
+    }
+
+    const data = await result.json();
+
+    return res.status(200).json({ count: data.users });
+  } catch (error) {
+    console.error("Error fetching user count:", error);
+    return res.status(200).json({ count: 477 });
+  }
 }
